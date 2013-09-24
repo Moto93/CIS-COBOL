2faad11cc68e3c72c51722d6ecf3301a76e0bc60
Moto93 <kyle.colie@gmail.com>
2013-09-24 15:05:36 -0400

    Added Solutions
    these are the solutions to my exercises for homework.

---
+++ Exercise 1/Exercise1.cbl
@@ -0,0 +1,41 @@
+      /####################################
+      /
+      /Program:    First Exercise
+      /Author:     CIS 111 Kyle Pennacchio 
+      /Status:     Working
+      / 
+      /#################################### 
+              
+       identification division.
+       program-id. Exercise1 as "Exercise1".
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+      /    Name 
+           display "Name: Kyle Pennacchio"
+      /    Age 
+           display "Age: 21"
+      /    Degree
+           display "Current Program Enrolled: CIS"
+      /    Semester
+           display "Current Semester: Second" 
+      /    Programming experience
+           display 'Programmining Experience: Basic knowledge of C#, C++
+      -    ', and Java.'
+            
+      /    Something interesting
+           display 'Something Interesting: I have seen one of my bones '
+      -    'before.' 
+           
+      /    **End the procedure and program by pressing Enter
+      /    **Used to keep console open while using Eclipse IDE   
+            stop "Press Enter to exit.".
+            stop run.
+      
+       
+
